41744366809a6ff6bfe45e0e9bd57e73b8f2f1ec
Ayemele Elgol <ayemeleelgol@gmail.com>
2025-08-23 11:45:33 +0100

    fix added tailwind css

--- MOTA/urls.py
+++ MOTA/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("mota_apps.users.urls", namespace="users"))
-]
+] + static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
