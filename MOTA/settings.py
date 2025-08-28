77e3498edf1df2e27ae38c3b0d25ae536a62b3a6
Ayemele Elgol <ayemeleelgol@gmail.com>
2025-08-28 13:35:27 +0100

    feat implemented translations

--- MOTA/settings.py
+++ MOTA/settings.py
@@ -54,8 +54,8 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -125,7 +125,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en'
 
 LANGUAGES = [
     ('en', 'English'),
--- MOTA/urls.py
+++ MOTA/urls.py
@@ -2,11 +2,19 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls.i18n import i18n_patterns
 from django.views.i18n import JavaScriptCatalog
 
 urlpatterns = [
+    # Language switching endpoint
+    path('i18n/', include('django.conf.urls.i18n')),
+    # JavaScript translations
     path('jsi18n/', JavaScriptCatalog.as_view(), name='javascript_catalog'),
+] + i18n_patterns(
+    # Admin URLs
     path('admin/', admin.site.urls),
-    path("", include("mota_apps.users.urls", namespace="users")),
-    path("", include("mota_apps.finance.urls", namespace="finance")),
-] + static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
+    # App URLs with namespaces
+    path('', include('mota_apps.users.urls', namespace='users')),
+    path('', include('mota_apps.finance.urls', namespace='finance')),
+    # Add prefix_untranslated=True if you want these URLs to be accessible without a language prefix
+) + static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0] if settings.STATICFILES_DIRS else settings.STATIC_ROOT)
