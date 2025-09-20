82341c9a8bd837ef1f538e6ae0990a272b95a97d
Ayemele Elgol <ayemeleelgol@gmail.com>
2025-09-20 16:45:50 +0100

    fix: javascript

--- MOTA/settings.py
+++ MOTA/settings.py
@@ -102,20 +102,20 @@ WSGI_APPLICATION = 'MOTA.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": config("DB_NAME", "postgres"),
-#         "USER": config("DB_USER", "postgres"),
-#         "PASSWORD": config("DB_PASSWORD", "postgres"),
-#         "HOST": config("DB_HOST", "db"),
-#         "PORT": config("DB_PORT", "5432"),
-#     },
-# }
-
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": config("DB_NAME", "postgres"),
+        "USER": config("DB_USER", "postgres"),
+        "PASSWORD": config("DB_PASSWORD", "postgres"),
+        "HOST": config("DB_HOST", "db"),
+        "PORT": config("DB_PORT", "5432"),
+    },
 }
+
+# DATABASES = {
+#     'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
