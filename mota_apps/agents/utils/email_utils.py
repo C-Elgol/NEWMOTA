df1faeb0433c29d1179d4e341aff65c7d15e0cfd
Ayemele Elgol <ayemeleelgol@gmail.com>
2025-09-20 18:57:04 +0100

    fix: javascript

--- mota_apps/agents/utils/email_utils.py
+++ mota_apps/agents/utils/email_utils.py
@@ -13,12 +13,12 @@ def send_email_with_logo(subject: str, plain_text: str, html_content: str, from_
     email.attach_alternative(html_content, "text/html")
 
     # Attach logo with Content-ID
-    logo_path = os.path.join(settings.STATIC_ROOT, 'images', 'Logo.bmp')
+    logo_path = os.path.join(settings.STATIC_ROOT, 'images', 'Logo.webp')
     if os.path.exists(logo_path):
         with open(logo_path, 'rb') as img:
             mime_image = MIMEImage(img.read())
             mime_image.add_header('Content-ID', '<logo_image>')
-            mime_image.add_header('Content-Disposition', 'inline', filename='Logo.bmp')
+            mime_image.add_header('Content-Disposition', 'inline', filename='Logo.webp')
             email.attach(mime_image)
 
     email.send(fail_silently=False)
